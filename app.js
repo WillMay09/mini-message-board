ce4490675dedb8b0630d0ea46bd49b69fbb7cebe
William <wmayhood@outlook.com>
2024-07-17 15:11:16 -0400

    Create connection to mongoDB database and created author model
    The author model serves to define how authors will be organized in the database collection

--- app.js
+++ app.js
@@ -2,7 +2,27 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const path = require('path');
+const mongoose = require("mongoose");
+
 //const messages = require('./routes/messageRoutes.js');
+
+mongoose.set("strictQuery", false);
+//username mayhoodwilliam
+//password sink2327
+const mongoDB = "mongodb+srv://mayhoodwilliam:sink2327@booklibrary.x8ludyp.mongodb.net/?retryWrites=true&w=majority&appName=bookLibrary";
+main().catch((err)=>console.log(err));
+async function main(){
+
+    await mongoose.connect(mongoDB);
+};
+
+const exampleModelSchema = new mongoose.Schema({
+
+    a_string: String,
+    a_date: Date,
+
+});
+const SomeModel = mongoose.model("SomeModel", exampleModelSchema);
 
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
